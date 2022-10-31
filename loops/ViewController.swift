77896c277ffc10c24633964158fa5ba996e8f221
Esmay <Molinaesme729@gmail.com>
2022-10-31 10:32:26 -0600

    addid buttons in stack view

--- loops/ViewController.swift
+++ loops/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var idk: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button1(_ sender: Any) {
+        
+        var fruit = ["apple","pear","p[peach","strawberry"]
+        
+        
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+    }
+    
+    @IBAction func button4(_ sender: Any) {
+    }
+    
+    @IBAction func button5(_ sender: Any) {
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
