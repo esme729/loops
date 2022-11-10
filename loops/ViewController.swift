d8355ae3d7ad478d3c7d09d480c4d047eb9020e7
Esmay <Molinaesme729@gmail.com>
2022-11-10 10:45:52 -0700

    mvp

--- loops/ViewController.swift
+++ loops/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var idk: UITextView!
     
@@ -18,32 +18,58 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func button1(_ sender: Any) {
         
         var fruit = ["apple","pear","p[peach","strawberry"]
         
-var output = ""
+        var output = ""
         
-for i in fruit{
+        for i in fruit{
             output += "\(i)\n"
         }
         idk.text = output
     }
     
     @IBAction func button2(_ sender: Any) {
+        
+        var veggies = ["tomato","carrot","pea","celery"]
+        
+        var output = ""
+        
+        for veggie in 0...3 {
+            output += "\(veggies[veggie])\n"
+        }
+        idk.text = output
+        
     }
-    
     @IBAction func button3(_ sender: Any) {
+        
+        var drinks = ["tea","jumex","jarritos","boba"]
+        
+        var output = ""
+        for i in 0..<4{ output += "\(drinks[i])\n"
+        }
+        idk.text = output
     }
     
-    @IBAction func button4(_ sender: Any) {
-    }
+    @IBAction func button4(_ sender: Any) {var snacks = ["chips","candy","icecream","water"]
+        var output = ""
+        for i in 0..<snacks.count{output+="\(snacks[i])\n"}
+        idk.text = output }
+    
+    
     
     @IBAction func button5(_ sender: Any) {
+        var contact = ["Name":"tom","Address":"123FekeSteeet","Phone":"123-1234"]
+        
+        var output = ""
+        for (key,value) in contact{output += "\(key): \(value)\n"
+        }
+        idk.text = output
     }
     
-    @IBAction func clear(_ sender: Any) {
+    @IBAction func clear(_ sender: Any) {idk.text = ""
     }
     
     
